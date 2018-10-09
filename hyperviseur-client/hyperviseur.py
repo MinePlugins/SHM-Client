4f5ae987547a8687828f63ab293632f5f7e86247
Quentin COURNUT <quentin.cournut@estiam.com>
2018-10-09 14:48:19 +0200

    Correction du bug crontab et sudo

--- hyperviseur-client/hyperviseur.py
+++ hyperviseur-client/hyperviseur.py
@@ -68,22 +68,6 @@ def logger(msg, type="info"):
     f.close()
 
 
-def datawrite():
-    global data
-    tsyslog.sendUpdate()
-    logger("Modification des données", "warning")
-    with open(PATH_DATA, 'w') as outfile:
-        json.dump(data, outfile)
-
-
-def monitoringwrite():
-    global monitoring
-    logger("Modification des monitorings", "warning")
-    with open(PATH_MONITORING, 'w') as outfile:
-        print(monitoring)
-        json.dump(monitoring, outfile)
-
-
 def startup():
     global data
     global monitoring
@@ -102,9 +86,12 @@ def startup():
         with open(PATH_MONITORING, 'w', encoding='utf-8'):
             pass
         monitoring = json.loads(DEFAULT_MONITORING)
-        monitoringwrite()
     else:
-        monitoring = json.load(open(PATH_MONITORING))
+        try:
+            monitoring = json.load(open(PATH_MONITORING))
+        except:
+            monitoring = json.loads(DEFAULT_MONITORING)
+
     if not os.path.exists(PATH_LOG):
         with open(PATH_LOG, 'w', encoding='utf-8'):
             pass
@@ -114,14 +101,31 @@ def startup():
         with open(PATH_DATA, 'w', encoding='utf-8'):
             pass
         data = json.loads(DEFAULT_CONFIG)
-        datawrite()
     else:
-        data = json.load(open(PATH_DATA))
+        try:
+            data = json.load(open(PATH_DATA))
+        except:
+            data = json.loads(DEFAULT_CONFIG)
+
 
 def encrypt_string(text):
     sha_signature = \
         hashlib.sha256(text.encode()).hexdigest()
     return sha_signature
+
+def datawrite():
+    global data
+    tsyslog.sendUpdate()
+    logger("Modification des données", "warning")
+    with open(PATH_DATA, 'w') as outfile:
+        json.dump(data, outfile)
+
+def monitoringwrite():
+    global monitoring
+    logger("Modification des monitorings", "warning")
+    with open(PATH_MONITORING, 'w') as outfile:
+        print(monitoring)
+        json.dump(monitoring, outfile)
 
 def measure_temps():
     if osn == "TB":
